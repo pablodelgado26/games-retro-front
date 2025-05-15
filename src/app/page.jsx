914cd8b972e7c8ee3031431b4d20f73d12b9b156
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-05-15 08:22:35 -0300

    feat: create project games retro

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Games Retro</h1>
+      <p>Registros de Jogos Retro</p>
+    </main>
+  );
+}
